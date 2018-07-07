76fc6c1b41c35679e7dde867f208f2456d2ab2cd
Eduardo Uliana <edu02_rko@hotmail.com>
2018-07-07 08:57:02 -0300

    Adicionado projeto

---
+++ ex-boot-web/src/main/java/br/edu/unoescsmo/exbootweb/ExBootWebApplication.java
@@ -0,0 +1,12 @@
+package br.edu.unoescsmo.exbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBootWebApplication.class, args);
+	}
+}
